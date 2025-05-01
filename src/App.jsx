2129a51e51bf01fae34d7719ac8530623be499d4
bytesandroses <isaacabodunrin05@gmail.com>
2025-05-01 17:12:29 +0200

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="logo-react" alt="React logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
 
         <h1>Hello from Amplify</h1>
       </header>
     </div>
-  )
+  );
 }
 
 export default App;
